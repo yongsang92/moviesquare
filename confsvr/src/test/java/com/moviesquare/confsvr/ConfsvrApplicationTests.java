fa41a17173de70237dbb7443f1652dc9d201fc2c
yong <yongsang92@kakao.com>
2020-10-26 19:24:20 +0900

    linking to github

---
+++ confsvr/src/test/java/com/moviesquare/confsvr/ConfsvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviesquare.confsvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfsvrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ japan/src/main/java/com/moviesquare/japan/JapanApplication.java
@@ -0,0 +1,13 @@
+package com.moviesquare.japan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapanApplication.class, args);
+	}
+
+}
